695427795cd86a945c0dcf5fe7d37a03149a5df6
Steve Novosel <novosel991@gmail.com>
2018-09-01 21:15:11 -0400

    refactor of landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
-import Button from './components/Inputs/Button/index.jsx';
-import InputText from './components/Inputs/InputText/index.jsx';
-import InputTextArea from './components/Inputs/InputTextArea/index.jsx';
-import InputCheckBox from './components/Inputs/InputCheckBox/index.jsx';
-import Card from './components/Blocks/Card/index.jsx';
 import { Switch, Route } from 'react-router-dom';
+
 import { Landing } from './components/Landing/index.jsx';
+import ComponentLibrary from './components';
 
 import styles from './App.scss';
 
@@ -15,46 +12,7 @@ class App extends Component {
     return (
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/components" render={() => {
-          return (
-            <div styleName="app-container">
-              <h1>Inputs</h1>
-              <h4>Buttons</h4>
-              <Button label="check" styleType="wire" />
-              <Button label="mate" styleType="wire" />
-              <Button label="seconday" styleType="secondary" />
-              <Button label="primary" styleType="primary" />
-              <Button label="warning" styleType="tertiary" />
-              <br />
-              <h4>Input Text</h4>
-              <InputText placeholder="Enter this here..." />
-              <br />
-              <h4>Input Text Area </h4>
-              <InputTextArea placeholder="Enter text in this text area box..." />
-              <h4>Check boxes </h4>
-              <InputCheckBox />
-              <br />
-              <h1>Layouts</h1>
-              <br />
-              <h4>Card</h4>
-              <section styleName="card-showcase">
-                <div>
-                  <h6>Card Regular</h6>
-                  <Card />
-                </div>
-                <div>
-                  <h6>Card Tall</h6>
-                  <Card styleType="tall" />
-                </div>
-                <div>
-                  <h6>Card Wide</h6>
-                  <Card styleType="wide" />
-                </div>
-              </section>
-              <br />
-            </div>
-          );
-        }} />
+        <Route path="/components" component={ComponentLibrary} />
         <Route path="/about" render={() => (
           <p>About</p>
         )} />
---
+++ src/components/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
+
+import Button from './Inputs/Button/index.jsx';
+import InputText from './Inputs/InputText/index.jsx';
+import InputTextArea from './Inputs/InputTextArea/index.jsx';
+import InputCheckBox from './Inputs/InputCheckBox/index.jsx';
+import Card from './Blocks/Card/index.jsx';
+
+import styles from './style.scss';
+
+class ComponentLibrary extends Component {
+
+  render() {
+    return (
+      <div styleName="app-container">
+        <h1>Inputs</h1>
+        <h4>Buttons</h4>
+        <Button label="check" styleType="wire" />
+        <Button label="mate" styleType="wire" />
+        <Button label="seconday" styleType="secondary" />
+        <Button label="primary" styleType="primary" />
+        <Button label="warning" styleType="tertiary" />
+        <br />
+        <h4>Input Text</h4>
+        <InputText placeholder="Enter this here..." />
+        <br />
+        <h4>Input Text Area </h4>
+        <InputTextArea placeholder="Enter text in this text area box..." />
+        <h4>Check boxes </h4>
+        <InputCheckBox />
+        <br />
+      </div>
+    );
+  }
+
+}
+
+const ComponentLibraryStyled = CSSModules(ComponentLibrary, styles);
+
+export default ComponentLibraryStyled;
